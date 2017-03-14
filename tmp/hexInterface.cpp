4080815c0f058253d815d1786180b15136a712bf
Tony Di Malta <Sky-Angels8@hotmail.fr>
2017-03-14 15:08:06 +0100

    Add mouse input
    Add mouse input
    Fix window positionning

--- tmp/hexInterface.cpp
+++ tmp/hexInterface.cpp
@@ -25,7 +25,7 @@ void glDrawHexagonGrid(float x, float y, float z, int ni, int nj) {
     glColor3f(0, 0, 0);
     int i, j;
     float x0;
-    x -= float(ni-1) * hexagon_gx * 0.5; // just shift x, y to start position (i=0, j=0)
+    x -= float(ni-1) * hexagon_gx * 0.5;
     x -= float(nj-1) * hexagon_dx * 0.5;
     y -= float(nj-1) * hexagon_gy * 0.5;
     for(j = 0, x0 = x; j < nj; j++, x0 += hexagon_dx, x = x0, y += hexagon_gy) {
@@ -45,6 +45,14 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
     }
 }
 
+static void mouse_button_callback(GLFWwindow* window, int key, int action, int mods) {
+    double xpos, ypos;
+    if(key == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        glfwGetCursorPos(window, &xpos, &ypos);
+        std::cout << "Mouse pos : (" << xpos << ", " << ypos << ")" << std::endl;
+    }
+}
+
 void glPerspective(GLdouble fovY, GLdouble aspect, GLdouble zNear, GLdouble zFar) {
     GLdouble fH = tan(fovY / 360 * M_PI) * zNear,
     		 fW = fH * aspect;
@@ -52,8 +60,15 @@ void glPerspective(GLdouble fovY, GLdouble aspect, GLdouble zNear, GLdouble zFar
     glFrustum(-fW, fW, -fH, fH, zNear, zFar);
 }
 
+void glInit() {
+    glMatrixMode(GL_PROJECTION);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+}
+
 int main() {
-	GLFWwindow* window;
+	GLFWwindow *window;
 
 	glfwSetErrorCallback(error_callback);
 	glfwInit();
@@ -66,6 +81,7 @@ int main() {
 		return -1;
 	}
 	glfwSetKeyCallback(window, key_callback);
+	glfwSetMouseButtonCallback(window, mouse_button_callback);
 	glfwMakeContextCurrent(window);
 	if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
 		std::cout << "Failed to initialize OpenGL context" << std::endl;
@@ -73,33 +89,31 @@ int main() {
 	}
 	glfwSwapInterval(1);
 
-	const GLFWvidmode * mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-	int screenWidth = mode->width, screenHeight = mode->height;
+	const GLFWvidmode *mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+	int screenWidth = mode->width, screenHeight = mode->height, length = 11;
+
+    GLint windowWidth, windowHeight;
+    glfwGetWindowSize(window, &windowWidth, &windowHeight);
+    glfwSetWindowPos(window, (screenWidth - windowWidth) / 2, (screenHeight - windowHeight) / 2);
+	glInit();
 
 	while(!glfwWindowShouldClose(window)) {
 		// Scale to window size
-		GLint windowWidth, windowHeight;
 		glfwGetWindowSize(window, &windowWidth, &windowHeight);
-		glfwSetWindowPos(window, (screenWidth - windowWidth) / 2, (screenHeight - windowHeight) / 2);
 		glViewport(0, 0, windowWidth, windowHeight);
 
 		// Draw stuff
-		glClearColor(1.0, 1.0, 1.0, 1.0);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		glMatrixMode(GL_PROJECTION_MATRIX);
+		glClear(GL_COLOR_BUFFER_BIT);
 		glLoadIdentity();
 		glPerspective(60, (double)windowWidth / (double)windowHeight, 0.1, 100);
-
-		glMatrixMode(GL_MODELVIEW_MATRIX);
-
-		glDrawHexagonGrid(0, 0, -6, 11, 11);
+		glDrawHexagonGrid(0, 0, -ceil(length)/2, length, length);
 
 		// Update Screen
+		glFlush();
 		glfwSwapBuffers(window);
 
-		// Check for any input, or window movement
-		glfwPollEvents();
+		// Wait for any input, or window movement
+		glfwWaitEvents();
 	}
 	glfwDestroyWindow(window);
 	glfwTerminate();

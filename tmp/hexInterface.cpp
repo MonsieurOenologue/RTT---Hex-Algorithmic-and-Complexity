e53457bc0731b1d0bfd4d8ddfd6b7f260477e55f
Tony Di Malta <Sky-Angels8@hotmail.fr>
2017-03-15 04:56:07 +0100

    Add new board + mouse acquisition
    New board implementation to handle mouse input.
    Add mouse button left to get the position on board.

--- tmp/hexInterface.cpp
+++ tmp/hexInterface.cpp
@@ -4,34 +4,51 @@
 #include <iostream>
 #include <math.h>
 
-#define hexagon_r	0.2
-#define hexagon_dx	hexagon_r * cos(30.0 * M_PI / 180.0)
-#define hexagon_dy	hexagon_r * sin(30.0 * M_PI / 180.0)
-#define hexagon_gx	2.0 * hexagon_dx
-#define hexagon_gy	2.0 * hexagon_dx * sin(60.0 * M_PI / 180.0)
+char factor = 1, length = 11;
+float offsetX = 0, offsetY = 0;
 
-void glDrawHexagon(float x, float y, float z) {
+float hex_corner_angle(char i) {
+    short angle_deg = 60 * i + 30;
+    return M_PI / 180 * angle_deg;
+}
+
+float hex_cornerX(float angle_rad, float x) {
+    return length * factor * cos(angle_rad) + x;
+}
+
+float hex_cornerY(float angle_rad, float y) {
+    return length * factor * sin(angle_rad) + y;
+}
+
+void pixel_to_hex(double &x, double &y) {
+    double q = ((x + length * factor) * sqrt(3) / 3 - y / 3) / length / factor, r = y * 2 / 3 / length / factor;
+    y = round(q)-1;
+    x = round(r)-1;
+}
+
+void glDrawHexagon(float width, float height) {
+    float angle;
     glBegin(GL_LINE_LOOP);
-	    glVertex3f(x - hexagon_dx, y - hexagon_dy, z);
-	    glVertex3f(x - hexagon_dx, y + hexagon_dy, z);
-	    glVertex3f(x             , y + hexagon_r,  z);
-	    glVertex3f(x + hexagon_dx, y + hexagon_dy, z);
-	    glVertex3f(x + hexagon_dx, y - hexagon_dy, z);
-	    glVertex3f(x             , y - hexagon_r,  z);
+        for(char i = 0; i < 6; ++i) {
+            angle = hex_corner_angle(i);
+            glVertex2i(hex_cornerX(angle, width), hex_cornerY(angle, height));
+        }
     glEnd();
 }
 
-void glDrawHexagonGrid(float x, float y, float z, int ni, int nj) {
+void glDrawFlatI(int maxWidth, int maxHeight) {
+    float width = sqrt(3) * length * (length + 1 + length / 2), height = length * (length + 1) * 1.5;
+    char i, j;
+    for(factor = 1; width * (factor+1) < maxWidth && height * (factor+1) < maxHeight; ++factor);
     glColor3f(0, 0, 0);
-    int i, j;
-    float x0;
-    x -= float(ni-1) * hexagon_gx * 0.5;
-    x -= float(nj-1) * hexagon_dx * 0.5;
-    y -= float(nj-1) * hexagon_gy * 0.5;
-    for(j = 0, x0 = x; j < nj; j++, x0 += hexagon_dx, x = x0, y += hexagon_gy) {
-        for(i = 0; i < ni; i++, x += hexagon_gx) {
-            glDrawHexagon(x, y, z);
+    offsetX = length * 1.5 * factor;
+    offsetY = sqrt(3) * length * (3 / 2) * factor;
+    for(i = 0, height = offsetX; i < length; ++i) {
+        for(j = 0, width = i * sqrt(3) / 2 * length * factor + offsetY; j < length; ++j) {
+            glDrawHexagon(width, height);
+            width += sqrt(3) * length * factor;
         }
+        height += length * 1.5 * factor;
     }
 }
 
@@ -50,14 +67,9 @@ static void mouse_button_callback(GLFWwindow* window, int key, int action, int m
     if(key == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
         glfwGetCursorPos(window, &xpos, &ypos);
         std::cout << "Mouse pos : (" << xpos << ", " << ypos << ")" << std::endl;
+        pixel_to_hex(xpos, ypos);
+        std::cout << "Board pos : (" << xpos << ", " << ypos << ")" << std::endl;
     }
-}
-
-void glPerspective(GLdouble fovY, GLdouble aspect, GLdouble zNear, GLdouble zFar) {
-    GLdouble fH = tan(fovY / 360 * M_PI) * zNear,
-    		 fW = fH * aspect;
-
-    glFrustum(-fW, fW, -fH, fH, zNear, zFar);
 }
 
 void glInit() {
@@ -71,10 +83,13 @@ int main() {
 	GLFWwindow *window;
 
 	glfwSetErrorCallback(error_callback);
-	glfwInit();
+	if(!glfwInit()) {
+		std::cout << "Failed to initialize glfw context" << std::endl;
+		return -1;
+	}
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	window = glfwCreateWindow(640, 640, "Hexxxor3000", NULL, NULL);
+	window = glfwCreateWindow(650, 400, "Hexxxor3000", NULL, NULL);
 	if(!window) {
 		std::cout << "Failed to create window context" << std::endl;
 		glfwTerminate();
@@ -90,7 +105,7 @@ int main() {
 	glfwSwapInterval(1);
 
 	const GLFWvidmode *mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-	int screenWidth = mode->width, screenHeight = mode->height, length = 11;
+	int screenWidth = mode->width, screenHeight = mode->height;
 
     GLint windowWidth, windowHeight;
     glfwGetWindowSize(window, &windowWidth, &windowHeight);
@@ -105,8 +120,24 @@ int main() {
 		// Draw stuff
 		glClear(GL_COLOR_BUFFER_BIT);
 		glLoadIdentity();
-		glPerspective(60, (double)windowWidth / (double)windowHeight, 0.1, 100);
-		glDrawHexagonGrid(0, 0, -ceil(length)/2, length, length);
+
+		glOrtho(0, windowWidth, windowHeight, 0, 0, 1);
+		glDrawFlatI(windowWidth, windowHeight);
+
+		// Enable vertex table
+        /*glEnableClientState(GL_VERTEX_ARRAY);
+        glEnableClientState(GL_COLOR_ARRAY);
+
+        // Send data
+        glVertexPointer(P_SIZE, GL_FLOAT, 0, pos);
+        glColorPointer(C_SIZE, GL_FLOAT, 0, colors);
+
+        // Draw stuff
+        glDrawElements(GL_LINE_STRIP, N_VERTS, GL_UNSIGNED_INT, index);
+
+        // Disable vertex table
+        glDisableClientState(GL_COLOR_ARRAY);
+        glDisableClientState(GL_VERTEX_ARRAY);*/
 
 		// Update Screen
 		glFlush();

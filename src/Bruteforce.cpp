ca996fe011ff4ca83900ac85dafb7f43e98a24a0
RudyBardout <rudybardout@orange.fr>
2017-04-03 23:43:24 +0200

    [ADD] Beggining of BruteForce ( Alpha )

--- src/Bruteforce.cpp
+++ src/Bruteforce.cpp
@@ -1,4 +1,6 @@
 #include "Bruteforce.h"
+#include "Action.h"
+#include "HexBoard.h"
 /**
  *  @file    Bruteforce.cpp
  *  @author  RTT
@@ -10,6 +12,43 @@
  *  @section DESCRIPTION
  *
  */
-Bruteforce::Bruteforce() {}
+bool Bruteforce::Bruteforce() {
+    vector<string> TMP = HexBoard.board;
+    if(player == playerL) {
+        for(char i = 0; i < HexBoard.nbPawnsPlayed; i++) {
+            for(char j = 0; j < HexBoard.nbPawnsPlayed - i; j++){
+                for(char x = 0; x < HexBoard.length; x++) {
+                    for(char y = 0; y < HexBoard.length; y++) {
+                        if(TMP[x][y] == ' ') {
+                            if(!HexBoard.victoryByRecursion(x,y)) {
+                            TMP[x][y] = x;
+                            }
+                        else setPosition(x,y,'x');
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+    else {for(char i = 0; i < HexBoard.nbPawnsPlayed; i++) {
+            for(char j = 0; j < HexBoard.nbPawnsPlayed - i; j++){
+                for(char x = 0; x < HexBoard.length; x++) {
+                    for(char y = 0; y < HexBoard.length; y++) {
+                        if(TMP[x][y] == ' ') {
+                            if(!HexBoard.victoryByRecursion(x,y)) {
+                            TMP[x][y] = x;
+                            }
+                        else setPosition(x,y,'o');
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+
+    }
+}
 
 Bruteforce::~Bruteforce() {}
--- src/Bruteforce.h
+++ src/Bruteforce.h
@@ -15,7 +15,7 @@ private :
 
 
 public :
-            Bruteforce();
+            bool Bruteforce();
             ~Bruteforce();
 };
 
